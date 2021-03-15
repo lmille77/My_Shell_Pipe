d6613367ca040f6225a24fa176fb2a3368c59e75
Logan Miller <miller4277@gmail.com>
2021-03-15 23:08:59 +0000

    Improved memory management and removed redundancy.

--- main.cpp
+++ main.cpp
@@ -15,31 +15,39 @@ int main()
 {	
 	while (1)
 	{
-		char* args0[MAXLINE/2+1],* args1[MAXLINE / 2 + 1], * args2[MAXLINE / 2 + 1]; 
-		char command1[MAXLINE / 2], command2[MAXLINE / 2];
+		char* args1[MAXLINE / 2 + 1];	
 		int pid1, pid2;
-		char* p_cmd1, * p_cmd2;
 
+		//prompt input, read input and store into "input"
 		printf("mysh>");
 		fflush(stdout);
-
 		char input[MAXLINE];
 		fgets(input, MAXLINE, stdin);
-
+    
+		//stops program once user types in "exit"
 		if (!strcmp(input, "exit\n"))
 		{
 			break;
 		}
 
+		//sets last value of input = 0
+		//strtok uses this value to terminate
+		//allows program to know when to stop parsing
 		input[strlen(input) - 1] = 0;
 
+    
 		if (strstr(input, "|"))
-		{			
-
+		{	
+      char* args2[MAXLINE/2 + 1];		
+			char* p_cmd1, * p_cmd2;
 			char first[MAXLINE / 2], last[MAXLINE / 2];
+
+			//parse input before and after "|"
 			strcpy(first, strtok(input, "|"));
-			strcpy(last, strtok(NULL, "|"));			
-      //parse input before "|", set values for execvp
+			strcpy(last, strtok(NULL, "|"));
+
+			//parse first half of input delimited by " "
+			//set values used for execvp
 			p_cmd1 = strtok(first, " ");
 			int i = 0;
 			while (p_cmd1 != NULL)
@@ -47,29 +55,31 @@ int main()
 				args1[i++] = strdup(p_cmd1);
 				p_cmd1 = strtok(NULL, " ");
 			}
-      args1[i] = NULL;
-			strcpy(command1, args1[0]);	
-			//parse input after "|", set values for execvp
+			args1[i] = NULL;			
+
+			//parse last half of input delimited by " "
+			//set values used for execvp
 			p_cmd2 = strtok(last, " ");
 			int j = 0;
 			while (p_cmd2 != NULL)
 			{
 				args2[j++] = strdup(p_cmd2);
-				p_cmd2 = strtok(NULL, " \n");
+				p_cmd2 = strtok(NULL, " ");
 			}
-      args2[j] = NULL;
-			strcpy(command2, args2[0]);
+			args2[j] = NULL;			
 			
+			//create pipe
 			int pipefd[2];
 			pipe(pipefd);
       
+			//fork child process
 			pid1 = fork();
 
 			if (pid1 == 0)
 			{
 				close(pipefd[0]);
 				dup2(pipefd[1], 1);
-				execvp(command1, args1);
+				execvp(args1[0], args1);
 			}
 			else
 			{
@@ -78,7 +88,7 @@ int main()
 				{
 					close(pipefd[1]);
 					dup2(pipefd[0], 0);
-					execvp(command2, args2);
+					execvp(args2[0], args2);
 				}
 			}
 			close(pipefd[1]);
@@ -89,18 +99,18 @@ int main()
 		}
 		else
 		{
-			int child_pid = fork();
-			if (child_pid == 0)
+			pid1 = fork();
+			if (pid1 == 0)
 			{				
 				char* token = strtok(input, " ");
 				int i = 0;
 				while (token != NULL)
 				{
-					args0[i++] = token;
+					args1[i++] = token;
 					token = strtok(NULL, " ");
 				}
-				args0[i] = NULL;
-				execvp(args0[0], args0);
+				args1[i] = NULL;
+				execvp(args1[0], args1);
 				exit(0);
 			}
 			else
